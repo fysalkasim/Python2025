067ab3ab8f06c4612c20bceaf3d91fea0f5f1eb5
fysalkasim <fysalkasim@gmail.com>
2024-10-09 19:16:08 +0530

    modules

--- Eric Matthes - Python Crash Course-No Starch Press (2023)/PRACTICE/data.py
+++ Eric Matthes - Python Crash Course-No Starch Press (2023)/PRACTICE/data.py
@@ -1,10 +1,10 @@
 def data_science(*tools):
     for i in tools:
-        print(f"I am using {i} for my data analaysis works")
+        print(f"I am using {i} for my data analysis works")
 
-def completed_calss(thislist):
+def completed_class(thislist):
     processed_orders = []
     while thislist:
         present_order = thislist.pop(0)
-        print("processing=",present_order)
+        print("processing=", present_order)
         processed_orders.append(present_order)
