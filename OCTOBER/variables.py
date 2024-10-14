887960cd957f5ced8f13ab47a4bea31fc48ba813
fysalkasim <fysalkasim@gmail.com>
2024-10-14 14:40:06 +0530

    variables

---
+++ OCTOBER/variables.py
@@ -0,0 +1,25 @@
+# print("Variables")
+# name = "Fysal"
+# print(name)
+# name = "Ku"
+# print(name)
+# NAME = "FYSAL"
+# print(NAME)
+# first_name = "Fysal"
+# last_name = "Kasim"
+# print(first_name)
+# print(last_name)
+# print(first_name,last_name)
+# _full_name = "Fysal kasim"
+# print(_full_name)
+# 1st_name = "fysal"
+# print(1st_name)
+# course = "Data analytics"
+# print(course)
+# print = "python"
+# print(print)
+# print("hello world")
+name = "Fysal"
+print(name)
+del name
+print(name)
