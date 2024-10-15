59ed4b81ffe9d9ea99179385d03b21dcee7237e4
fysalkasim <fysalkasim@gmail.com>
2024-10-15 13:05:17 +0530

    strings

--- OCTOBER/datatypes.py
+++ OCTOBER/datatypes.py
@@ -1,2 +1,48 @@
+# language = "Python"
+# print(language)
+# language = 'Python'
+# print(language)
+# language = '''Python'''
+# print(language)
+# language = """Python"""
+# print(language)
+# # language = python
+# # print(language)
+# number = 42
+# print(number)
+# print(type(number))
+# number = '42'
+# print(number)
+# print(type(number))
+
+# var1 = 45
+# var2 = 12.05
+# var3 = True
+# var4 = False
+# var5 = [1,2,3,4,5]
+# var6 = (5,6,7,8,9,10)
+# var7 = "Hello world"
+
+# course = "Data science"
+# print(len(course))
 
 
+# slicer = "Learning python"
+# print(slicer[0])
+# print(slicer[9])
+# print(slicer[14])
+# print(slicer[-1])
+# print(slicer[-15])
+# print(slicer[0:8])
+# print(slicer[9:15])
+
+
+# print(slicer[:])
+thistring = "python is a programming language"
+# print(thistring[12:23])
+# print(thistring[14:23:2])
+# print(thistring[::])
+# print(thistring[::1])
+# print(thistring[::2])
+# print(thistring[::3])
+print(thistring[-1::-1])
