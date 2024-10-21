f47704641889819c4ff9e1c7b8ce8975f797a5d6
fysalkasim <fysalkasim@gmail.com>
2024-10-21 14:05:26 +0530

    operators

--- OCTOBER/Numbers and operators/numbers_operators.py
+++ OCTOBER/Numbers and operators/numbers_operators.py
@@ -7,9 +7,102 @@
 
 # print(f"We have datatypes of  {type(intvar)},{type(floatvar)},{type(complexvar)},{type(boolvar)}")
 
-num1 = 10
-num2 = 5
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(num1**num2)
+# num1 = 10
+# num2 = 5
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1**num2)
+#Assignment 9
+# birth_year=2000
+# current_year=2024
+# current_age=current_year-birth_year
+# print(f"my current age is {current_age}")
+
+# #Assignment 10
+# height=1.69
+# weight=51
+# bmi=weight/(height**2)
+# print(f"I am salu and age is {current_age} and my BMI level is {bmi}")
+
+# #Assignment 11
+# degree_celcious=28
+# fahrenheit=(degree_celcious*(9/5)+32)
+# print(f"Today temperature is {fahrenheit}F")
+
+
+# #assignment 9
+# birth_year = 1990
+# current_year = 2024
+# print(f"Age : {current_year - birth_year}")
+
+# #assignment 10
+# height = 1.65
+# weight = 65
+# print(f"Name : David\nAge : 50\nBMI level : {weight/(height ** 2)}Kg/m^2")
+
+# #assignment 11
+# temp_in_celsius = 37
+# print(f"Today's temperature : {(temp_in_celsius *(9/5)) + 32} F")
+
+# #ASSIGNMENT 9
+
+# year_of_birth = 2000
+# current_year = 2024
+# print(f"Hi I'm Sreekutty. \nMy age is {current_year - year_of_birth}.")
+
+# #ASSIGNMENT 10
+
+# height = 1.69
+# weight = 58
+# BMI = weight / (height ** 2)
+# print(f"Name : Sreekutty S Kumar \nAge : 24 \nBMI : {BMI}")
+
+# #ASSIGNMENT 11
+
+# temp_in_celsius = 30
+# fahrenheit = (temp_in_celsius*(9/5)+32)
+# print(f"Todays temperature : {fahrenheit}")
+
+# # Assignment 9
+# year=1991
+# current_age=2024-year
+# print(f"My current age is {current_age}")
+
+
+# # assignment 10
+# name="John"
+# Age=30
+# height=1.76
+# weight=60
+# BMI= weight/height**2
+# print(f"Name:{name} \nAge:{Age} \nBMI level={BMI}")
+
+# # assignment 11
+# today_temp=28
+# fahrenheit=today_temp*9/5+32
+# print(f"today's temperature is {fahrenheit}°F")
+# var = "Python" 
+# print("n" not in var)
+# string = "Python Programming" 
+# print(string[::2])
+# string2 = "Hi welcome to python programming"
+# sub = "python"
+# print(sub in string2)
+
+# print(not True)
+# print(True and True)
+# print(True and False)
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+# print(not(True or False))
+# str1 = "Hi"
+# str2 = "Hi"
+# print(str2 is str1)
+# a = 5
+# b = 7
+# print(a is b)
+
+
