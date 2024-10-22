fdb43e2b877365f9e1ab3289d64e5873bee831fa
fysalkasim <fysalkasim@gmail.com>
2024-10-22 13:08:03 +0530

    list

---
+++ OCTOBER/List/List_operations.py
@@ -0,0 +1,32 @@
+names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
+
+age = [10,12,14,16]
+
+print(type(names))
+print(type(age))
+
+print(names)
+print(age)
+
+print(names[-1])
+
+names = ["Reethu","Sreelekshmi","Sreekutty","Salu","Lekshmi"]
+
+print(len(names))
+
+
+course = "Data_Analytics"
+
+print(list(course))
+
+course = list(course)
+print(course)
+
+del names[-1]
+print(names)
+
+numbers = [1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6]
+print(sorted(numbers))
+
+names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
+print(sorted(names))
