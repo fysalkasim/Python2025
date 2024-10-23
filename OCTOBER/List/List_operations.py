e6e710e00cbe13e083edd0ff2ad9642aeac85600
fysalkasim <fysalkasim@gmail.com>
2024-10-23 13:06:24 +0530

    list methods

--- OCTOBER/List/List_operations.py
+++ OCTOBER/List/List_operations.py
@@ -1,43 +1,94 @@
-names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
+# names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
 
-age = [10,12,14,16]
+# age = [10,12,14,16]
 
-print(type(names))
-print(type(age))
+# print(type(names))
+# print(type(age))
 
-print(names)
-print(age)
+# print(names)
+# print(age)
 
-print(names[-1])
+# print(names[-1])
 
-names = ["Reethu","Sreelekshmi","Sreekutty","Salu","Lekshmi"]
+# names = ["Reethu","Sreelekshmi","Sreekutty","Salu","Lekshmi"]
 
-print(len(names))
-
-
-course = "Data_Analytics"
-
-print(list(course))
-
-course = list(course)
-print(course)
-
-del names[-1]
-print(names)
+# print(len(names))
 
 
+# course = "Data_Analytics"
 
-names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
-print(sorted(names))
-numbers = [1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6]
+# print(list(course))
 
-print(numbers)
-print(len(numbers))
+# course = list(course)
+# print(course)
 
-numbers[2:7] = [10,11,21]
-print(numbers)
-print(len(numbers))
+# del names[-1]
+# print(names)
 
-numbers.append(5)
-print(numbers)
+
+
+# names = ["Reethu","Sreelekshmi","Sreekutty","Salu"]
+# print(sorted(names))
+# numbers = [1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6]
+
+# print(numbers)
+# print(len(numbers))
+
+# numbers[2:7] = [10,11,21]
+# print(numbers)
+# print(len(numbers))
+
+# numbers.append(5)
+# print(numbers)
+
+
+#Use of membership operators (in, not in)
+
+# player_names = ["Rohit","Dhoni","Virat","Rahul","Bumrah"]
+
+# print("Rohit" in player_names)
+
+# print("sachin" in player_names)
+# player_names = ["Rohit","Dhoni","Virat","Rahul","Bumrah"]
+# print(player_names[1])
+
+# player_names[1] = "Sanju samson"
+# print(player_names)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# print(thislist)
+# # thislist[1:3] = ["Jack fruit", "watermelon"]
+# # print(thislist)
+# thislist[1:3] = ["Jack fruit", "watermelon", "Pappaya"]
+# print(thislist)
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["Jack fruit"]
+# print(thislist)
+# player_names = ["Rohit","Dhoni","Virat","Rahul","Bumrah"]
+# player_names.insert(1,"Sanju samson")
+# print(player_names)
+
+basket = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+new_fruits = ["Jack fruit", "watermelon"]
+
+basket.extend(new_fruits)
+print(basket)
+
+letters = "ABCDEFGHIJKL"
+
+new_fruits.extend(letters)
+
+print(new_fruits)
+
+basket = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+print(basket)
+basket.remove("kiwi")
+print(basket)
+
+basket = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+print(basket)
+#remove the mango from basket
+
+basket.pop()
+print(basket)
 
