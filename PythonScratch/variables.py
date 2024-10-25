da10a71766b678b3895dcc639a4c9d0a31144040
fysalkasim <fysalkasim@gmail.com>
2024-10-25 15:01:57 +0530

    regex

---
+++ PythonScratch/variables.py
@@ -0,0 +1,26 @@
+a = "hello world"
+a = "Hello python"
+print(a)
+
+#Varibale name
+#use meaningfull variable name
+#letters,underscore(_),numbers,special charcters
+
+message = "hello world"
+print(message)
+
+number = 10
+letter = "123324"
+number_1 = 1.0
+print(number)
+print(letter)
+
+
+
+print(type(number))
+print(type(letter))
+print(type(number_1))
+
+
+del number
+print(number)
