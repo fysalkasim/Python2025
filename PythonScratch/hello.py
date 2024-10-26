8a7ed45028d68c289019f9c3f1cd83fef8b20439
fysalkasim <fysalkasim@gmail.com>
2024-10-26 10:01:42 +0530

    if

--- PythonScratch/hello.py
+++ PythonScratch/hello.py
@@ -1,1 +1,2 @@
-print("hello world")
+name = "suhail"
+print(type(name))
--- PythonScratch/variables.py
+++ PythonScratch/variables.py
@@ -1,26 +1,39 @@
-a = "hello world"
-a = "Hello python"
-print(a)
+# a = "hello world"
+# a = "Hello python"
+# print(a)
 
-#Varibale name
-#use meaningfull variable name
-#letters,underscore(_),numbers,special charcters
+# #Varibale name
+# #use meaningfull variable name
+# #letters,underscore(_),numbers,special charcters
 
-message = "hello world"
-print(message)
+# message = "hello world"
+# print(message)
 
-number = 10
-letter = "123324"
-number_1 = 1.0
-print(number)
-print(letter)
-
-
-
-print(type(number))
-print(type(letter))
-print(type(number_1))
+# number = 10
+# letter = "123324"
+# number_1 = 1.0
+# print(number)
+# print(letter)
 
 
-del number
-print(number)
+
+# print(type(number))
+# print(type(letter))
+# print(type(number_1))
+
+
+# del number
+# print(number)
+# a = "HI"
+# b = 5
+# c = 5.011
+# d = True   #boolean
+# e = 3 + 4j #complex
+#list,tuples,set, dictionaries,
+
+message = "hi"
+name = input("Enter the name: ")
+print(message, name)
+
+
+
