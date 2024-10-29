5660f3cc24a89d3aacb95dc6c3e6aa84ace2c434
fysalkasim <fysalkasim@gmail.com>
2024-10-29 16:13:39 +0530

    numbers

---
+++ OCTOBER/Numbers and operators/numbers.py
@@ -0,0 +1,77 @@
+# complex_number = 3 +4j
+# print(complex_number.imag)
+# print(complex_number.real)
+# number_1 = 10
+# number_2 = 3
+# print(number_1+number_2)
+# print(number_1-number_2)
+# print(number_1*number_2)
+# print(number_1**number_2)
+# print(number_1/number_2)
+# print(number_1//number_2)
+# print(number_1%number_2)
+# a = "D"
+# b = "D"
+# # print(a,b)
+
+
+# print(2<=2)
+
+
+# a = 7
+# b = 10
+# a **= 5   #a = a+5
+# print(a)
+#Arithametic
+#Comparison
+#Assignmnet
+#Logical
+#
+# a = 10
+# a = 5+10
+
+# print( 10 == 10)
+# a = 10
+# b = 10
+# print(a == b)
+# print(a is b)
+
+# print(a is not b)
+
+
+#creatig a string for using membership operators
+
+# calculator
+
+
+
+# numbers = "123456789"
+# print("6" in numbers)
+# print("6" not in numbers)
+
+# print()
+# input()
+# type()
+# int()
+
+
+# keyword
+
+# del
+# and
+# or
+# not
+# is
+# is not
+# in
+
+
+
+# num_1 = int(input("Enter the first number:"))
+# num_2 = int(input("Enter the second number:"))
+# sum = num_1 + num_2
+# print(sum)
+# product = num_1*num_2
+# print(product)
+
+
