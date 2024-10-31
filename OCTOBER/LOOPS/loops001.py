acd2f05d3781064d3cb5a5066bf17f16d44c43bc
fysalkasim <fysalkasim@gmail.com>
2024-10-31 15:02:52 +0530

    for loop

---
+++ OCTOBER/LOOPS/loops001.py
@@ -0,0 +1,22 @@
+language = "python"
+# for i in language:
+#     print(i)
+# for i in language:
+#     print(i,end=' ')
+# for i in range(10):
+#     print(language)
+
+# for number in range(25):
+#     print(number)
+
+
+# for i in [1,2,3]:
+#     print(i)
+# num = 6
+# for i in range(1,11):
+#     print(num,"*",i,"=",i*6)
+
+
+
+
+
