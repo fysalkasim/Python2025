cd9a23f3950d25930c6ddc4475e7ca61cd1522e4
fysalkasim <fysalkasim@gmail.com>
2024-10-31 16:12:24 +0530

    loops

--- OCTOBER/LOOPS/loops001.py
+++ OCTOBER/LOOPS/loops001.py
@@ -1,4 +1,4 @@
-language = "python"
+# language = "python"
 # for i in language:
 #     print(i)
 # for i in language:
@@ -16,7 +16,50 @@ language = "python"
 # for i in range(1,11):
 #     print(num,"*",i,"=",i*6)
 
+# num = int(input("Enter the number:"))
+# for i in range(1,num+1):
+#     print(i)
+# num = int(input("Enter the number="))
+# for i in range(1,num+1):
+#     if i % 2 == 0:
+#         print(i)
+
+# sum = 0
+# num = int(input("Enter the number:"))
+# for i in range(1,num+1):
+#     if i % 2 ==0:
+#         sum += i
+# print(sum)
+
+# input = 5
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# number  =  int(input("Enter the number: "))
+# for i in range(1,number+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+
+# first Loop:
+# for 2:
+#     print()
+#     for 1:
+#         print(1, end='')
 
 
-
+# language = "python"
+# for i in range(10):
+#     print("hello")
+# number  =  int(input("Enter the number: "))
+# for i in range(number+1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+# limit = int(input("Enter the limit:"))
+# for i in range(1,limit+1):
+#     for j in range(1,limit+1):
+#         print("*",end="")
+#     print()
 
