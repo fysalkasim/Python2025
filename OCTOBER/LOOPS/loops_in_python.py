ba389daf644602fe8f1a8eae3e18cbe043407419
fysalkasim <fysalkasim@gmail.com>
2024-11-06 13:07:19 +0530

    loops

--- OCTOBER/LOOPS/loops_in_python.py
+++ OCTOBER/LOOPS/loops_in_python.py
@@ -136,6 +136,128 @@ name = "FYSAL"
 # number of repeatition from user input
 # print(each fruit as insructed by the user)
 
+# limit = int(input("Enter the limit : "))
+# a = 0
+# b = 1
+# for i in range(limit):
+#     if i == 0:
+#         print(a,end= " ") 
+#     elif i == 1 :  
+#         print(b,end= " ") 
+#     else:
+#         c = a + b
+#         print(c,end= " ") 
+#         a = b
+#         b = c 
+# count = int(input("Enter number of rows : "))
+# for i in range(count,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+# pyramid
+# num=int(input("enter any number"))
+# for i in range(0,num):
+#     for j in range(num-i-1):
+#         print(" ", end="")
+#     for j in range(i+1):
+#         print('*',end=' ')
+#     print()
+# mul=1
+# n=int(input("Enter any num:")) 
+# for i in range(1,n+1):
+#     mul*=i
+# print(f"The factorial of {n} is {mul}")
+
+#1)Takes a list of numbers as input from the user and then separates and displays them as odd and even numbers
+# inputing_string =(input("Enter the numbers (Separated by spaces):"))
+# list_of_string = inputing_string.split()
+# print(list_of_string)
+# numbers = []
+# for i in list_of_string:
+#     numbers.append(int(i))
+# print(numbers)
+
+# odd=[]
+# even=[]
+# for i in numbers:
+#     if i%2==0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print("Even numbers:",even)
+# print("Odd Numbers:",odd)
+
+# sentence=input("Enter a sentence:")
+# vowels="aeiouAEIOU"
+# vowels_count=0
+# consonants_count=0
+
+# for char in sentence:
+#     if char.isalpha():
+#         if char in vowels:
+#             vowels_count += 1
+#         else:
+#             consonants_count += 1
+# print("No of vowels:",vowels_count)
+# print("No of consonants:",consonants_count)
+# squares = []
+# for i in range(1,11):
+#     squares.append(i**2)
+
+# print(squares)
+
+# number = list(range(1,101))
+# print(number)
+
+# square = [i**2 for i in range(1,101)]
+# print(square)
+# cubes = [i**3 for i in range(1,10)]
+# print(cubes)
+
+#
+# i=8
+# while i>0:
+#     print(i)
+#     i -= 1
+
+
+# i = 0
+# while i<=10:
+#     print("python")
+#     i += 1
+
+# i = 10
+# while i>=0:
+#     if i%2==0:
+#         print(i)
+#     i -= 1
 
 
 
+# count = int(input("Enter number of rows : "))
+# for i in range(0,count):  
+#     for j in range(count,0,-1):
+#         print(" ",end=" ")
+#     for k in range(i+1):
+#         print("*",end=" ")    
+#     print()
+
+# break,continue,pass
+
+# i = 0
+# while i < 30:
+#     if i == 10:
+#         pass
+#     print('Current number :', i)
+#     i += 1
+#else with while
+# i=0
+# while i<25:
+#     if i==5:
+#         break
+#     print(i)
+#     i=i+1
+# else:
+#     print(" no breaks")
+
+    
