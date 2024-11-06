d2167295aba3e10d33ea1f6dcc68211b4717b5ac
fysalkasim <fysalkasim@gmail.com>
2024-11-06 11:10:57 +0530

    python

--- Python CC/CHAPTER 5/try_it_yourself2.py
+++ Python CC/CHAPTER 5/try_it_yourself2.py
@@ -8,7 +8,7 @@ print("is the number is not equal to 200 is,", number != 200)
 
 print("The number is 3005 and the car is BMW is",(number == 3005) and (cars == 'BMW'))
 print("The number is 5000 and the car is BMW is",(number == 5000) and (cars == 'BMW'))
-print("The number is 3005 or the car is BMW",(number == 3005)or (car == 'BMW'))
+print("The number is 3005 or the car is BMW",(number == 3005)or (cars == 'BMW'))
 
 digits = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 print("Is the number 7 in the digits",7 in digits)
