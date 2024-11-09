067f84d08c54a16216bf2c267cca61542f1f0006
fysalkasim <fysalkasim@gmail.com>
2024-11-09 09:54:42 +0530

    dictionaries

---
+++ OCTOBER/Dictionaries/dictionaries_python.py
@@ -0,0 +1,75 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+"brand" : "BMW",
+"year": 1900
+}
+# print(thisdict)
+# print(thisdict["year"])
+
+# for key in thisdict:
+#     print(thisdict[key])
+
+# print(thisdict["brand"])
+
+# print(thisdict.get("brand"))
+
+
+# for key,value in thisdict.items():
+#     print(f"The {key} have value of {value}")
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.update({"milage": 2020})
+# print(thisdict)
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.pop("year")
+# print(thisdict)
+
+# thislist = [[1,2],[3,4],[5,6]]
+# print(thislist[2][0])
+
+# data = [(1,2),(3,4),(5,6),(7,8)]
+
+# datadict = dict(data)
+# print(datadict)
+# thisdict.update(([5,2],[10,5]))
+# print(thisdict)
+# thisstring = 
+# if thisstring:
+#   print('Year is present')
+
+# thisdict.popitem()
+
+# Dictionary with various key types
+# my_dict = {
+#     "string_key": "String",
+#     42: "Integer",
+#     3.14: "Float",
+#     True: "Boolean",
+#     None: "NoneType",
+#     (1, 2): "Tuple",
+# }
+
+# print(my_dict[(1,2)])
+# print(my_dict[True])
+# print(my_dict[None])
+# data = [('sravan', 23), ('ojaswi', 15),
+#         ('rohith', 8), ('gnanesh', 4), ('bobby', 20)]
+ 
+# # print({key: value for (key, value) in data})
+
+# print(dict(data))
+
+
+
+
