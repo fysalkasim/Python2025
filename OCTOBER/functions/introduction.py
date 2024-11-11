39a3edb2b697a9b2cf785c8e850b20eacbf9a42a
fysalkasim <fysalkasim@gmail.com>
2024-11-11 10:02:36 +0530

    functions

---
+++ OCTOBER/functions/introduction.py
@@ -0,0 +1,26 @@
+def functionname(a,b):
+    return a+b
+
+functionname(5,4)
+
+
+def square(num):
+    return(num**2)
+
+add = functionname(5,4)
+print(square(add))
+
+def mul15(n1,n2=15):
+    return(f"The multplied of value 3 with 15 is {n1*n2}")
+
+print(mul15(3))
+
+print(mul15(n2=10,n1=2))
+
+
+def favoritefood(number,*food):
+    print("i need",number,"food")
+    for i in food:
+        print(f"i love {i}")
+
+favoritefood(5,4,3)
