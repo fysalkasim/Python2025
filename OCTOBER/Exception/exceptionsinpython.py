77a0b85e819ac6c0318cf4e419f13acd5063feda
fysalkasim <fysalkasim@gmail.com>
2024-11-20 10:41:25 +0530

    file

---
+++ OCTOBER/Exception/exceptionsinpython.py
@@ -0,0 +1,40 @@
+# name = "python"
+# print(name)
+
+# # print(10/0)
+
+# # print(name+5)
+
+# # thislist = [1,2,3,4,5,6]
+# # print(thislist[9])
+
+# # try:
+# #     a = int(input("Enter a:"))
+# #     b = int(input("Enter b:"))
+# #     c = a/b
+# #     print("result:",c)
+# #     print("hi..")
+# # except Exception as ex:
+# #     print("There is an error in the input",ex)
+# # # except ValueError:
+# # #     print("Can't divide with unknown value")
+
+# # print("outside exception")
+
+
+# try:
+#     age = int(input("Enter the age:"))
+
+#     if(age!=18):
+#         raise ValueError
+#     else:
+#         print("the age is valid")
+# except Exception:
+#     print("The age is not valid")
+#     print(Exception)
+
+# print("outside exception")
+
+# from math import statss
+
+print("hell world")
---
+++ OCTOBER/Tuples/tuples_operations2.py
@@ -0,0 +1,7 @@
+thistuple = (1,3,3)
+print(thistuple)
+
+thislist = [1]
+thistuple = (1)
+print(type(thistuple))
+
