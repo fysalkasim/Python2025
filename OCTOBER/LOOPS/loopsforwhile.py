a79df274ddb7a815d1e37c7be46355bbcdaae263
fysalkasim <fysalkasim@gmail.com>
2024-11-21 15:31:29 +0530

    dict

---
+++ OCTOBER/LOOPS/loopsforwhile.py
@@ -0,0 +1,73 @@
+# # numbers = "123456"
+# # for number in "123456":    #for iterating_variable in sequence: statement
+# #     print(number)
+# # for i in "python":
+# #     print(i,"is present")
+
+# for i in range(2,7,2):   #1,2,3,4  range(start,stop,step) #2,,4,,6
+#     print(i)
+#hashble string,integers,bool,
+# thisdict = {      
+#   : "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "year": 2020
+# }
+# print(thisdict)
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# print(thisdict["bran"])
+# # print(thisdict.get("bran"))
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict.items()
+# print(x)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.pop()
+
+# empt = {}
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for value in thisdict.items():
+#   for i in value:
+#     print(i)
+
+
+# thislist = [1,2,3,4,5,6]
+# # mult = []
+# # for i in thislist:
+# #     mult.append(i*2)
+
+# # print(mult)
+
+# square = [i**2 for i in thislist]  #list comprehension
+# print(square) 
+
+# #dictoinary compreshension
+# data = [('sravan', 23), ('ojaswi', 15),
+#         ('rohith', 8), ('gnanesh', 4), ('bobby', 20)]
+# # print(dict(data))
+
+# x = {key: value for key, value in data }
+# print(x)
