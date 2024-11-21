f1c0b4fb634b5b24ef0ee98d8345032f065d24af
fysalkasim <fysalkasim@gmail.com>
2024-11-21 10:15:57 +0530

    f

---
+++ OCTOBER/Set/setsofpython.py
@@ -0,0 +1,17 @@
+# thisset = ["jackfruit","apple", "banana", "cherry"]
+# # print(sorted(thisset))
+# thisset.extend("ABCD")
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.discard("banana")
+# print(thisset)
+# thisset.discard("banana")
+
+# print("hello")
+
+# emptyset = set()
+# print(type(emptyset))
+
+
