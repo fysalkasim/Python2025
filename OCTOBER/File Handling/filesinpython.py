ee10ad773bce5f2ff4cc220c3a73d56781df05a7
fysalkasim <fysalkasim@gmail.com>
2024-11-21 10:12:52 +0530

    pnadas

---
+++ OCTOBER/File Handling/filesinpython.py
@@ -0,0 +1,22 @@
+# fileptr = open(r"OCTOBER\File Handling\firstfile.txt","r")
+# if fileptr:
+#     print("file is opened successfully")
+# else:
+#     print("file not found")
+# with open(r"OCTOBER\File Handling\firstfile.txt","r") as f:
+#     print(f.read())
+
+# fileptr = open(r"OCTOBER\File Handling\firstfile.txt", "r")
+# print(fileptr.tell())
+# print(fileptr.read())
+# print(fileptr.tell())
+# overwriting the content to the file
+# fileptr.write("Again adding elements using a")
+# fileptr.close()
+# print(fileptr.read())
+# for i in fileptr:
+#     print(i)
+# closing the opened the file
+# fileptr.close()
+
+    
