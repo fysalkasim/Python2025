2893b1aeb395a7d90e62aedf2fa0a5018aaf5618
fysalkasim <fysalkasim@gmail.com>
2024-11-25 17:33:09 +0530

    python

---
+++ OCTOBER/functions/latest.py
@@ -0,0 +1,84 @@
+# print()
+# input()
+# type()
+# list()
+# set()
+# dict()
+# tuple()
+# int()
+# str()
+# sorted()
+# len()
+# range()
+
+# print(list(range(int(input()))))
+# del
+# print()
+
+# string = """"Welcome to the world of
+# python"""
+# def functionname():
+#     """docstring"""
+#     print("Just called my function")
+
+# functionname()
+# f(x) = x*5
+
+# x = parameter 
+# f(5)
+# 5 is an argument to the parameter x
+# 25
+# def double(number):
+#     """Gives the double of the given number"""
+#     return number*2
+# def triple(num):
+#     return num*3
+# name = inpu
+
+# def fun(a,b,c):
+#     return a*b*c
+# taking values from the user
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# def addition(num1, num2):
+#     return num1+num2
+# # printing the sum of a and b
+# print("Sum = ", addition(a, b))
+
+
+# def show(name,age,id):
+#     print(name)
+
+# show()
+# print("python","C++",sep ="******",end="")
+# print("java")
+
+
+# def favorite_movie(movie,year=2014):
+#     print(f"{movie} is my favorite movie in {year}")
+
+# favorite_movie(year=2015,movie="Intersteller")
+
+
+# def printme(*names):
+#     for i in names:
+#         print(i)
+
+# printme("john","David","smith","nick")
+
+# def information(name,**info):
+#     print(name)
+#     print(inf)
+
+# information("fysal",place="Kaloor",job="Data analyst",k)
+
+# year = 2025
+# def show_year():
+#     global year
+#     year = 2024 #local
+#     print(year)
+
+# show_year()
+#  #global
+# year = 2026
+# print(year)
